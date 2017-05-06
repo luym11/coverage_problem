e11b41737706a2de7d1614dcaf17401406e7def0
imoe <lym602@126.com>
2017-05-06 17:27:10 +0300

    agent score higher while at resource location, reduce by sidecoverCoeff while beside resource location

--- get_agentscore.m
+++ get_agentscore.m
@@ -7,6 +7,8 @@ n = size(Map, 2);
 
 V_ON = Map(x, y) * CoverageMap(x, y);
 
+sidecoverCoeff = 0.8; 
+
 % % Agent only knows himself
 % if( x + 1 < 11)
 %     V_ON = V_ON + Map(x+1, y);
@@ -23,16 +25,16 @@ V_ON = Map(x, y) * CoverageMap(x, y);
 
 % Agent knows all neighbours (in 2 hops)
 if( x  < m)
-    V_ON = V_ON + Map(x+1, y) * CoverageMap(x+1, y);
+    V_ON = V_ON + Map(x+1, y) * CoverageMap(x+1, y) * sidecoverCoeff;
 end
 if( y  < n)
-    V_ON = V_ON + Map(x, y+1) * CoverageMap(x, y+1);
+    V_ON = V_ON + Map(x, y+1) * CoverageMap(x, y+1) * sidecoverCoeff;
 end
 if( x - 1 > 0)
-    V_ON = V_ON + Map(x-1, y) * CoverageMap(x-1, y); 
+    V_ON = V_ON + Map(x-1, y) * CoverageMap(x-1, y) * sidecoverCoeff; 
 end
 if( y - 1 > 0)
-    V_ON = V_ON + Map(x, y-1) * CoverageMap(x, y-1);
+    V_ON = V_ON + Map(x, y-1) * CoverageMap(x, y-1) * sidecoverCoeff;
 end
 
 V_OFF = 0; 
